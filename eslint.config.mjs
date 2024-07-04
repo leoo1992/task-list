cbfcf76330c8d6b14b43b8a1bcfbf9c9e41a65bc
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-07-03 23:10:46 -0300

    init:  config inicial

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import react from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      react,
+    },
+  },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];

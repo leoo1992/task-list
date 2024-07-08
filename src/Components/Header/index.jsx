2fff429cc66debba0b48aa6beca3c7621078dfdc
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-07-08 00:40:31 -0300

    add header scroll effect

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,23 +1,33 @@
 /* eslint-disable no-unused-vars */
 import styles from './Header.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BurgButton from './SubComponents/BurgButton';
 import NavLinks from './SubComponents/NavLinks';
 import Logo from './SubComponents/Logo';
 import { ToggleNavbar } from '../../utils/ToggleNavbar';
+import HeaderScrollEffect from '../../utils/HeaderScrollEffect';
 
 export default function Header() {
   const [isActive, setIsActive] = useState(false);
   const toggleNavbar = ToggleNavbar({ setIsActive, isActive });
+  const [isVisible, setIsVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
   return (
-    <nav className={styles.navbar}>
-      <Logo />
-      <NavLinks
-        toggleNavbar={toggleNavbar}
-        isActive={isActive}
+    <>
+      <HeaderScrollEffect
+        prevScrollPos={prevScrollPos}
+        setPrevScrollPos={setPrevScrollPos}
+        setIsVisible={setIsVisible}
       />
-      <BurgButton toggleNavbar={toggleNavbar} />
-    </nav>
+      <nav className={`${styles.navbar} ${isVisible ? '' : styles.hidden}`}>
+        <Logo />
+        <NavLinks
+          toggleNavbar={toggleNavbar}
+          isActive={isActive}
+        />
+        <BurgButton toggleNavbar={toggleNavbar} />
+      </nav>
+    </>
   );
 }

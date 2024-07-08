02a72abd2d9e11595b38831cee0e1d86f3cd43e9
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-07-08 00:41:22 -0300

    add resize drop down effect and styles

--- src/Components/TaskList/index.jsx
+++ src/Components/TaskList/index.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
+import { useState } from 'react';
 import { useTaskContext } from '../../Contexts/TaskContext';
 import Loader from '../../Components/Loader';
 import TaskItem from '../../Components/TaskItem';
 import ErrorPage from '../../Pages/ErrorPage';
+import styles from './TaskList.module.css';
+import ResizeDropDownEffect from '../../utils/ResizeDropDownEffect';
 
 export default function TaskList() {
   const { taskList, loading, error } = useTaskContext();
+  const [openDropdownId, setOpenDropdownId] = useState(null);
 
   if (loading) {
     return <Loader />;
@@ -16,12 +20,20 @@ export default function TaskList() {
   }
 
   return (
-    <>
+    <div className={styles.taskList}>
       {taskList.map((task) => (
-        <div key={task.id}>
-          <TaskItem task={task} />
+        <div
+          className={styles.itemTaskList}
+          key={task.id}
+        >
+          <ResizeDropDownEffect setOpenDropdownId={setOpenDropdownId} />
+          <TaskItem
+            task={task}
+            isDropdownOpen={openDropdownId === task.id}
+            setOpenDropdownId={setOpenDropdownId}
+          />
         </div>
       ))}
-    </>
+    </div>
   );
 }

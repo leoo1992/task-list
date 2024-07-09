296e75d75ff8e78714c4572b0818a2525c5da427
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-07-09 02:28:38 -0300

    FINAL MAIN

--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './GlobalCSS/global.css';
 import GlobalRouters from './Routes/GlobalRoutes';
+import { Toaster } from 'react-hot-toast';
+import { ToastProvider } from './Contexts/ToastContext';
+import { TaskProvider } from './Contexts/TaskContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalRouters />
+    <ToastProvider>
+      <TaskProvider>
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            duration: 2500,
+            style: {
+              transitionDuration: '1000ms',
+            },
+            maxToasts: 2,
+          }}
+        />
+        <GlobalRouters />
+      </TaskProvider>
+    </ToastProvider>
   </React.StrictMode>,
 );
